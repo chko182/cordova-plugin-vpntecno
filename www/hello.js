dcfd8c253990907ee69f7438affc55f952f78906
AmitLT <amit@menagenet.co.il>
2015-07-11 09:56:34 +0300

    dev

--- www/hello.js
+++ www/hello.js
@@ -2,6 +2,7 @@
 
 module.exports = {
     greet: function (name, successCallback, errorCallback) {
+      console.log("hey1");
         cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
     }
 };
